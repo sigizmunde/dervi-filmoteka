39605ea38d920ad7ed63291dedf1b69a6d05e148
ShenyaLipinsky <shenyalipinskiy@gmail.com>
2022-06-07 00:33:53 +0300

    Update markup.js

--- src/js/markup.js
+++ src/js/markup.js
@@ -5,22 +5,35 @@ import { API_IMG_URL, refs } from './global';
 import { parseGenresByString } from './movies';
 
 export function showMovies(responseData) {
-    responseData.results.map(movie => {
-        refs.cardsBox.innerHTML += `
+  responseData.results.map(movie => {
+    refs.cardsBox.innerHTML += `
           <li class="card">
             <a href="" class="card-link" movie-id="${movie.id}">
-              <img
-                src="${API_IMG_URL}${movie.poster_path}"
-                class="card-image"
-                alt=""
-              />
+              <div class="card-button-slider">
+                <img
+                  src="${API_IMG_URL}${movie.poster_path}"
+                  class="card-image"
+                  alt=""
+                />
+                <div class="card-button-background">
+                  <button class="card-button in-watched">watched</button>
+                  <button class="card-button in-queue">queue</button>
+                </div>
+              </div>
+              <div class="card-label-wrapper">
+                <div class="card-label-in-watched"></div>
+                <div class="card-label-in-queue"></div>
+              </div>
               <div class="card-body">
                 <p class="card-title"><b>${movie.original_title}</b></p>
-                <p class="card-genres"><b>${parseGenresByString(movie.genre_ids, 2)} | ${movie.release_date.substr(0, 4)}</b></p>
+                <p class="card-genres"><b>${parseGenresByString(
+                  movie.genre_ids,
+                  2
+                )} | ${movie.release_date.substr(0, 4)}</b></p>
               </div>
             </a>
-          </li>`
-      });
+          </li>`;
+  });
 }
 
 export function showMovieInfo() {}
