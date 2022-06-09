d70bd6fe48661e7bdcb4ee27a9ff72e8ceb57ce9
belinskiy-and <90721066+belinskiy-and@users.noreply.github.com>
2022-06-09 13:22:36 +0300

    code-ref

--- src/js/markup.js
+++ src/js/markup.js
@@ -4,37 +4,31 @@ const cardSection = document.querySelector('.card-section');
 import { API_IMG_URL, refs } from './global';
 import { parseGenresByString } from './movies';
 
-export function showMovies(responseData) {
-  responseData.results.map(movie => {
-    const genreString = parseGenresByString(movie.genre_ids, 2);
-    refs.cardsBox.innerHTML += `
-          <li class="card">
-            <a href="" class="card-link" movie-id="${movie.id}">
-              <div class="card-button-slider">
-                <img
-                  src="${API_IMG_URL}${movie.poster_path}"
-                  class="card-image"
-                  alt=""
-                />
-                <div class="card-button-background">
-                  <button class="card-button in-watched">watched</button>
-                  <button class="card-button in-queue">queue</button>
-                </div>
+export function showMovies(movie) {
+  refs.cardsBox.innerHTML += `
+        <li class="card ${movie.wachedOrQueueClass}">
+          <a href="" class="card-link" movie-id="${movie.id}">
+            <div class="card-button-slider">
+              <img
+                src="${movie.posterPath}"
+                class="card-image"
+                alt=""
+              />
+              <div class="card-button-background">
+                <button class="card-button in-watched">watched</button>
+                <button class="card-button in-queue">queue</button>
               </div>
-              <div class="card-label-wrapper">
-                <div class="card-label-in-watched"></div>
-                <div class="card-label-in-queue"></div>
-              </div>
-              <div class="card-body">
-                <p class="card-title"><b>${movie.original_title}</b></p>
-                <p class="card-genres"><b>${genreString} | ${movie.release_date.substr(
-      0,
-      4
-    )}</b></p>
-              </div>
-            </a>
-          </li>`;
-  });
+            </div>
+            <div class="card-label-wrapper">
+              <div class="card-label-in-watched"></div>
+              <div class="card-label-in-queue"></div>
+            </div>
+            <div class="card-body">
+              <p class="card-title"><b>${movie.title}</b></p>
+              <p class="card-genres"><b>${movie.genresInRow} | ${movie.releaseDate}</b></p>
+            </div>
+          </a>
+        </li>`;
 }
 
 export function showMovieInfo() {}
--- src/js/movies.js
+++ src/js/movies.js
@@ -1,10 +1,91 @@
+/*
+      ОПИСАНИЕ РАБОТЫ МОДУЛЯ
+
+      В модуле прописан класс "Movie" для работы с каждой карточкой фильма
+
+      --Получить список трендов (популярных фильмов за неделю):
+
+        В модуле "controller.js", при инициализации (функция "init()"") визвана функция "getMovieList()".
+
+*/
+
 // main module to manipulate with data inside an application
 
-import { API_KEY, API_BASE_URL, API_IMG_URL, refs } from './global';
+import { API_KEY, API_BASE_URL, API_IMG_URL, NOPOSTER_IMG_URL, refs, watchedIdArr, queueIdArr } from './global';
 // import { fetchMovie, fetchMovies, getGenres } from 'movie-api';
 import { showMovies } from './markup';
 import APIService from './movie-api';
+import * as initialGenres from './dummy-array-objs/genres.json';
 // import DataStorage from './data.js';
+
+class Movie {
+  constructor(responseData) {
+    // console.log(responseData);
+    this.id = responseData.id;
+    this.posterPath = this.#getPosterPath(responseData.poster_path);
+    this.title = responseData.original_title;
+    this.genres = responseData.genre_ids;
+    this.releaseDate = responseData.release_date.substr(0, 4);
+    this.inWached = this.#getInWached();
+    this.inQueue = this.#getInQueue();
+  }
+
+  // Genres in row
+  get genresInRow() {
+    return this.#parseGenresByString(2);
+  }
+
+  get wachedOrQueueClass() {
+    return this.inWached ? "in-watched" : this.inQueue ? "in-queue" : "";
+  }
+
+  // Private methods
+
+  #parseGenresByString(maxCount = 0) {
+    const genreList = this.#getGenres();
+    const genreNames = [];
+
+    for (let i = 0; i < this.genres.length; i++) {
+      if (maxCount && i === maxCount && i < this.genres.length - 1) {
+        genreNames.push('others');
+        break;
+      }
+
+      const findValue = genreList.find(item => item.id === this.genres[i]);
+
+      if (findValue) {
+        genreNames.push(findValue.name);
+      }
+    }
+
+    return genreNames.join(', ');
+  }
+
+  #getInWached() {
+    return !!watchedIdArr.find(item => item === this.id);
+  }
+
+  #getInQueue() {
+    return !!queueIdArr.find(item => item === this.id);
+  }
+
+  #getGenres() {
+  return API.getGenres();
+  }
+
+  #getPosterPath(poster_path) {
+    const fullPosterPatch = `${API_IMG_URL}${poster_path}`;
+    return fullPosterPatch;
+    // return NOPOSTER_IMG_URL;
+
+    // const poster = new Image();
+    // poster.src = fullPosterPatch;
+    // 
+    // poster.onload = () => fullPosterPatch;
+    // poster.onerror = () => alert("NoImage");
+  }
+
+}
 
 // const dataStorage = new DataStorage(API_KEY);
 const API = new APIService();
@@ -15,9 +96,35 @@ export function getMovieList(params) {
   // depending on params requests API or data
   if (!params) {
     API.getTrending()
-      .then(responseData => {
-        showMovies(responseData);
+      .then(responseData => {        
+        console.log(`Current page: ${responseData.page}, total page: ${responseData.total_pages}`); // --> for pagination
+        return responseData.results;
       })
+      .then(movieList => {
+        movieList.map(movieItem => {          
+          const movie = new Movie(movieItem); // class instance          
+          showMovies(movie);
+        })
+        
+        // Получаем все селекторы с классом ".card-link", это ссылки, для открытия деталей фильма
+        refs.cardLinks = document.querySelectorAll('.card-link');
+
+        // console.log(refs.cardLinks); // ВРЕМЕННО
+
+        // Перебираем все селекторы и для каждого навязываем событие 'click'
+        refs.cardLinks.forEach(cardLink => {
+          // console.log(cardLink); // ВРЕМЕННО
+          cardLink.addEventListener('click', () => {
+            event.preventDefault();
+            console.log(cardLink); // ВРЕМЕННО
+
+            // Открываем модальное окно (убираем класс "is-hidden")
+            // cardLink.toggleAttribute(".is-hidden");
+              
+            });
+          });
+
+      })      
       .catch(result => console.log(result));
   }
 }
@@ -30,34 +137,6 @@ function search(params) {
   // depending on params searches films in current list
 
   return fetchMovies();
-}
-
-import * as initialGenres from './dummy-array-objs/genres.json';
-
-function getGenres() {
-  // genreList = getGenres();
-  const genreList = API.getGenres();
-  return genreList;
-}
-
-export function parseGenresByString(genre_ids = [], maxCount = 0) {
-  const genreList = getGenres();
-  const genreNames = [];
-
-  for (let i = 0; i < genre_ids.length; i++) {
-    if (maxCount && i === maxCount && i < genre_ids.length - 1) {
-      genreNames.push('others');
-      break;
-    }
-
-    const findValue = genreList.find(item => item.id === genre_ids[i]);
-
-    if (findValue) {
-      genreNames.push(findValue.name);
-    }
-  }
-
-  return genreNames.join(', ');
 }
 
 function addQueue(film) {
