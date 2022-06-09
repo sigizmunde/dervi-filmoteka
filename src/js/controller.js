1a581e17608d8edee10fe2603efa4ff0fe2e61aa
illya bondar <1816579@gmail.com>
2022-06-10 00:52:08 +0300

    renamed openModal to openTeamModal

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,11 +2,11 @@
 
 import { API_KEY, refs } from './global';
 import { getMovieList } from './movies';
-// import { openModal } from './modal';
+import { openModal } from './modal';
 
 export function init() {
   //refs, event listeners, genres request, popular movies request
-  
+
   refs.cardsBox = document.querySelector('.cards-box');
   refs.header = document.querySelector('.header');
   refs.homeLink = document.querySelector('#home');
@@ -20,19 +20,19 @@ export function init() {
   refs.movieModal = document.querySelector('.modal');
 
   try {
-  refs.logo.addEventListener('click', onHomeLinkClick);
-  refs.homeLink.addEventListener('click', onHomeLinkClick);
-  refs.libraryLink.addEventListener('click', onLibraryLinkClick);
-  refs.libraryWatchBtn.addEventListener('click', onLibraryWatchBtnClick);
-  refs.libraryQueBtn.addEventListener('click', onLibraryQueBtnClick);
-  refs.ourTeamLink.addEventListener('click', openModal);
-  refs.closeModalBtn.addEventListener('click', closeModal);
-  refs.backdrop.addEventListener('click', onBackdropClick);
-  // refs.movieModal.addEventListener('click', onCloseClick);
+    refs.logo.addEventListener('click', onHomeLinkClick);
+    refs.homeLink.addEventListener('click', onHomeLinkClick);
+    refs.libraryLink.addEventListener('click', onLibraryLinkClick);
+    refs.libraryWatchBtn.addEventListener('click', onLibraryWatchBtnClick);
+    refs.libraryQueBtn.addEventListener('click', onLibraryQueBtnClick);
+    refs.ourTeamLink.addEventListener('click', openTeamModal);
+    refs.closeModalBtn.addEventListener('click', closeTeamModal);
+    refs.backdrop.addEventListener('click', onBackdropClick);
+    // refs.movieModal.addEventListener('click', onCloseClick);
   } catch (error) {
     console.log(error);
-    }
-  
+  }
+
   getMovieList();
 
   // before getMovieList()
@@ -46,9 +46,7 @@ export function init() {
   //     event.preventDefault();
   //     console.log(refs.cardLink)
   //   });
-  // });  
-
-
+  // });
 }
 
 function onHomeLinkClick(event) {
@@ -76,25 +74,24 @@ function onLibraryQueBtnClick() {
   refs.libraryWatchBtn.classList.remove('accent-btn');
 }
 
-
-function openModal() {
-    window.addEventListener('keydown', checkKeyPress);
-    document.body.classList.add('modal-open');
+function openTeamModal() {
+  window.addEventListener('keydown', checkKeyPress);
+  document.body.classList.add('modal-open');
 }
 
-function closeModal() {
-    window.removeEventListener('keydown', checkKeyPress);
-    document.body.classList.remove('modal-open');
+function closeTeamModal() {
+  window.removeEventListener('keydown', checkKeyPress);
+  document.body.classList.remove('modal-open');
 }
 
 function checkKeyPress(event) {
-    if (event.code === 'Escape') {
-        closeModal();
-    }
+  if (event.code === 'Escape') {
+    closeTeamModal();
+  }
 }
 
 function onBackdropClick(event) {
-    if (event.currentTarget === event.target) {
-        closeModal();
-    }
+  if (event.currentTarget === event.target) {
+    closeTeamModal();
+  }
 }
