7a2253f9e4ebf952aa1eb7646baaf4549db38971
belinskiy-and <90721066+belinskiy-and@users.noreply.github.com>
2022-06-09 06:42:34 +0300

    Add code refactoring && add movies flag

--- src/js/movies.js
+++ src/js/movies.js
@@ -1,10 +1,80 @@
 // main module to manipulate with data inside an application
 
-import { API_KEY, API_BASE_URL, API_IMG_URL, refs } from './global';
+import { API_KEY, API_BASE_URL, API_IMG_URL, NOPOSTER_IMG_URL, refs, watchedIdArr, queueIdArr } from './global';
 // import { fetchMovie, fetchMovies, getGenres } from 'movie-api';
 import { showMovies } from './markup';
 import APIService from './movie-api';
+import * as initialGenres from './dummy-array-objs/genres.json';
 // import DataStorage from './data.js';
+
+class Movie {
+  constructor(responseData) {
+    // console.log(responseData);
+    this.id = responseData.id;
+    this.posterPath = this.#getPosterPath(responseData.poster_path);
+    this.title = responseData.original_title;
+    this.genres = responseData.genre_ids;
+    this.releaseDate = responseData.release_date.substr(0, 4);
+    this.inWached = this.#getInWached();
+    this.inQueue = this.#getInQueue();
+  }
+
+  // Genres in row
+  get genresInRow() {
+    return this.#parseGenresByString(2);
+  }
+
+  get wachedOrQueueClass() {
+    return this.inWached ? "in-watched" : this.inQueue ? "in-queue" : "";
+  }
+
+  // Private methods
+
+  #parseGenresByString(maxCount = 0) {
+    const genreList = this.#getGenres();
+    const genreNames = [];
+
+    for (let i = 0; i < this.genres.length; i++) {
+      if (maxCount && i === maxCount && i < this.genres.length - 1) {
+        genreNames.push('others');
+        break;
+      }
+
+      const findValue = genreList.find(item => item.id === this.genres[i]);
+
+      if (findValue) {
+        genreNames.push(findValue.name);
+      }
+    }
+
+    return genreNames.join(', ');
+  }
+
+  #getInWached() {
+    return !!watchedIdArr.find(item => item === this.id);
+  }
+
+  #getInQueue() {
+    return !!queueIdArr.find(item => item === this.id);
+  }
+
+  #getGenres() {
+  return API.getGenres();
+  }
+
+  #getPosterPath(poster_path) {
+    const fullPosterPatch = `${API_IMG_URL}${poster_path}`;
+    return fullPosterPatch;
+    // return NOPOSTER_IMG_URL;
+
+    // const poster = new Image();
+    // poster.src = fullPosterPatch;
+    // 
+    // poster.onload = () => fullPosterPatch;
+    // poster.onerror = () => alert("NoImage");
+  }
+
+}
 
 // const dataStorage = new DataStorage(API_KEY);
 const API = new APIService();
@@ -15,9 +85,16 @@ export function getMovieList(params) {
   // depending on params requests API or data
   if (!params) {
     API.getTrending()
-      .then(responseData => {
-        showMovies(responseData);
+      .then(responseData => {        
+        console.log(`Current page: ${responseData.page}, total page: ${responseData.total_pages}`); // --> for pagination
+        return responseData.results;
       })
+      .then(movieList => {
+        movieList.map(movieItem => {          
+          const movie = new Movie(movieItem); // class instance          
+          showMovies(movie);
+        })        
+      })      
       .catch(result => console.log(result));
   }
 }
@@ -30,34 +107,6 @@ function search(params) {
   // depending on params searches films in current list
 
   return fetchMovies();
-}
-
-import * as initialGenres from './dummy-array-objs/genres.json';
-
-function getGenres() {
-  // genreList = getGenres();
-  const genreList = API.getGenres();
-  return genreList;
-}
-
-export function parseGenresByString(genre_ids = [], maxCount = 0) {
-  const genreList = getGenres();
-  const genreNames = [];
-
-  for (let i = 0; i < genre_ids.length; i++) {
-    if (maxCount && i === maxCount && i < genre_ids.length - 1) {
-      genreNames.push('others');
-      break;
-    }
-
-    const findValue = genreList.find(item => item.id === genre_ids[i]);
-
-    if (findValue) {
-      genreNames.push(findValue.name);
-    }
-  }
-
-  return genreNames.join(', ');
 }
 
 function addQueue(film) {
