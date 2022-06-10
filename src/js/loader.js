04a98c95a751e49141679df9dabb58b5f1a8decc
Olena Bondiuchna <93439766+elenbond@users.noreply.github.com>
2022-06-10 22:06:34 +0300

    fixed loader function

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,13 +1,15 @@
+import { refs } from "./global";
+refs.loader = document.querySelector('.lds-ripple');
 const DELAY = 500;
 
 export const showLoader = () => {
-    document.body.classList.add('loaded-hiding');
+    refs.loader.classList.add('loaded-hiding');
     console.log("loader");
 }
 
 export const hideLoader = () => {
-    document.body.classList.add('loaded');
-    document.body.classList.remove('loaded-hiding');
+    refs.loader.classList.add('loaded');
+    refs.loader.classList.remove('loaded-hiding');
 }
 
 // window.onload = function () {
