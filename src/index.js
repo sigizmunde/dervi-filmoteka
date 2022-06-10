3382a906d04e98adc1243cacc190c7d395b845db
illya bondar <1816579@gmail.com>
2022-06-10 20:28:23 +0300

    refactor data.js

--- src/index.js
+++ src/index.js
@@ -5,5 +5,24 @@ import { init } from './js/controller'; // movies
 document.addEventListener('DOMContentLoaded', init);
 
 //================================= test field =======================================
+import { DataStorage } from './js/data';
+const data = new DataStorage();
+const movieTempData = data.getWatched();
+console.log(movieTempData);
 
+data.addToWatched(51);
+console.log(data.getWatched());
+
+data.addToWatched(13);
+data.addToWatched(51);
+data.addToWatched(27);
+console.log(data.getWatched());
+
+data.removeFromWatched(15);
+data.removeFromWatched(13);
+data.addToWatched(18);
+data.addToWatched(26);
+data.removeFromWatched(26);
+data.addToWatched(33);
+console.log(data.getWatched());
 //====================================================================================
--- src/js/data.js
+++ src/js/data.js
@@ -1,48 +1,84 @@
- const KEY_WATCHED = 'Watched';
- const KEY_QUEUE = 'Queue';
-
 export class DataStorage {
-
-  constructor(apiKey) {
+  constructor() {
     // singleton pattern
     if (DataStorage._instance) {
       return DataStorage._instance;
     }
     DataStorage._instance = this;
     // singleton pattern
-
-    this.apiKey = apiKey;
   }
 
   getWatched() {
-
-      try {
-        const serializedData = localStorage.getItem('watched');
-        return serializedData === null ? [] : JSON.parse(serializedData);
-      } catch (err) {
-        console.error('Get state error: ', err);
-      }
-      return watchedArr;
-    
-  };
+    try {
+      const serializedData = localStorage.getItem('watched');
+      return serializedData === null ? [] : JSON.parse(serializedData);
+    } catch (err) {
+      console.error('Get state error: ', err);
+    }
+    return [];
+  }
 
   getQueue() {
-    
-      try {
-        const serializedData = localStorage.getItem('queue');
-        return serializedData === null ? [] : JSON.parse(serializedData);
-      } catch (err) {
-        console.error('Get state error: ', err);
+    try {
+      const serializedData = localStorage.getItem('queue');
+      return serializedData === null ? [] : JSON.parse(serializedData);
+    } catch (err) {
+      console.error('Get state error: ', err);
     }
-    return queueArr;
-    }
-    
-
-  setWatched(value) {
-    localStorage.setItem(KEY_WATCHED, JSON.stringify(value));
-  };
-
-  setQueue(value) {
-    localStorage.setItem(KEY_QUEUE, JSON.stringify(value))
+    return [];
   }
-};
+
+  #setWatched(watchedArr) {
+    try {
+      localStorage.setItem('watched', JSON.stringify(watchedArr));
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  #setQueue(queueArr) {
+    try {
+      localStorage.setItem('queue', JSON.stringify(queueArr));
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  addToWatched(id) {
+    const watchedArr = this.getWatched();
+    if (watchedArr.includes(id)) {
+      return;
+    }
+    watchedArr.push(id);
+    this.#setWatched(watchedArr);
+  }
+
+  removeFromWatched(id) {
+    const watchedArr = this.getWatched();
+    const index = watchedArr.indexOf(id);
+    if (index < 0) {
+      return;
+    }
+    watchedArr.splice(index, 1);
+    this.#setWatched(watchedArr);
+  }
+
+  addToQueue(id) {
+    const queueArr = this.getQueue();
+    if (queueArr.includes(id)) {
+      return;
+    }
+    queueArr.push(id);
+    this.#setQueue(queueArr);
+  }
+
+  removeFromQueue(id) {
+    const queueArr = this.getQueue();
+    const index = queueArr.indexOf(id);
+    if (index < 0) {
+      return;
+    }
+    queueArr.splice(index, 1);
+    this.#setQueue(queueArr);
+  }
+}
