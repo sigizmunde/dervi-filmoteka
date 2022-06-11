bf23bdd40e42b1351e5b073de44734179c6b6db1
Serhii Kravchenko <kravchenko087@ukr.net>
2022-06-11 18:21:40 +0300

    add switch theme

--- src/index.js
+++ src/index.js
@@ -13,11 +13,12 @@ const temporaryWatchedList = [299536, 383498, 500664, 466282, 455980, 73021];
 const temporaryQueueList = [489931, 454283, 421792, 429300, 857, 470918];
 
 for (let i = 0; i < temporaryWatchedList.length; i += 1) {
-  data.addToWatched(temporaryWatchedList[i]);
+    data.addToWatched(temporaryWatchedList[i]);
 }
 for (let i = 0; i < temporaryQueueList.length; i += 1) {
-  data.addToQueue(temporaryQueueList[i]);
+    data.addToQueue(temporaryQueueList[i]);
 }
 console.log(data.getWatched());
 console.log(data.getQueue());
+
 //====================================================================================
--- src/js/controller.js
+++ src/js/controller.js
@@ -4,110 +4,113 @@ import { API_KEY, refs } from './global';
 import { getMovieList, searchMovies } from './movies';
 import { openModal } from './modal';
 import { showLoader, hideLoader } from './loader';
+import { addClassDarkToHTML } from './switch-theme';
 
 export function init() {
-  //refs, event listeners, genres request, popular movies request
-  showLoader();
-  // hideLoader();
-  
-  
-  refs.cardsBox = document.querySelector('.cards-box');
-  refs.header = document.querySelector('.header');
-  refs.homeLink = document.querySelector('#home');
-  refs.libraryLink = document.querySelector('#library');
-  refs.logo = document.querySelector('#logo');
-  refs.libraryWatchBtn = document.querySelector('#lib-w');
-  refs.libraryQueBtn = document.querySelector('#lib-q');
-  refs.ourTeamLink = document.querySelector('#our-team');
-  refs.closeModalBtn = document.querySelector('[data-action="close-modal"]');
-  refs.backdrop = document.querySelector('.js-backdrop');
-  refs.movieModal = document.querySelector('.modal');
-  refs.searchForm = document.querySelector('#movie-search');
+    //refs, event listeners, genres request, popular movies request
+    addClassDarkToHTML()
+    showLoader();
+    // hideLoader();
 
-  try {
-    refs.logo.addEventListener('click', onHomeLinkClick);
-    refs.homeLink.addEventListener('click', onHomeLinkClick);
-    refs.libraryLink.addEventListener('click', onLibraryLinkClick);
-    refs.libraryWatchBtn.addEventListener('click', onLibraryWatchBtnClick);
-    refs.libraryQueBtn.addEventListener('click', onLibraryQueBtnClick);
-    refs.ourTeamLink.addEventListener('click', openTeamModal);
-    refs.closeModalBtn.addEventListener('click', closeTeamModal);
-    refs.backdrop.addEventListener('click', onBackdropClick);
-    refs.searchForm.addEventListener('submit', onMoviesSearch);
 
-    // refs.movieModal.addEventListener('click', onCloseClick);
-  } catch (error) {
-    console.log(error);
-  }
+    refs.cardsBox = document.querySelector('.cards-box');
+    refs.header = document.querySelector('.header');
+    refs.homeLink = document.querySelector('#home');
+    refs.libraryLink = document.querySelector('#library');
+    refs.logo = document.querySelector('#logo');
+    refs.libraryWatchBtn = document.querySelector('#lib-w');
+    refs.libraryQueBtn = document.querySelector('#lib-q');
+    refs.ourTeamLink = document.querySelector('#our-team');
+    refs.closeModalBtn = document.querySelector('[data-action="close-modal"]');
+    refs.backdrop = document.querySelector('.js-backdrop');
+    refs.movieModal = document.querySelector('.modal');
+    refs.searchForm = document.querySelector('#movie-search');
+    refs.checkboxDarkThemeRef = document.getElementById("checkbox");
+    refs.body = document.querySelector("body");
 
-  getMovieList();
-  // searchMovies();
+    try {
+        refs.logo.addEventListener('click', onHomeLinkClick);
+        refs.homeLink.addEventListener('click', onHomeLinkClick);
+        refs.libraryLink.addEventListener('click', onLibraryLinkClick);
+        refs.libraryWatchBtn.addEventListener('click', onLibraryWatchBtnClick);
+        refs.libraryQueBtn.addEventListener('click', onLibraryQueBtnClick);
+        refs.ourTeamLink.addEventListener('click', openTeamModal);
+        refs.closeModalBtn.addEventListener('click', closeTeamModal);
+        refs.backdrop.addEventListener('click', onBackdropClick);
+        refs.searchForm.addEventListener('submit', onMoviesSearch);
 
-  // before getMovieList()
-  // refs.cardLinks = document.querySelectorAll('.card-link');
+        // refs.movieModal.addEventListener('click', onCloseClick);
+    } catch (error) {
+        console.log(error);
+    }
 
-  // console.log(refs.cardLinks)
+    getMovieList();
+    // searchMovies();
 
-  // refs.cardLinks.forEach(cardLink => {
-  //   console.log(cardLink);
-  //   cardLink.addEventListener('click', () => {
-  //     event.preventDefault();
-  //     console.log(refs.cardLink)
-  //   });
-  // });
+    // before getMovieList()
+    // refs.cardLinks = document.querySelectorAll('.card-link');
+
+    // console.log(refs.cardLinks)
+
+    // refs.cardLinks.forEach(cardLink => {
+    //   console.log(cardLink);
+    //   cardLink.addEventListener('click', () => {
+    //     event.preventDefault();
+    //     console.log(refs.cardLink)
+    //   });
+    // });
 }
 
 function onHomeLinkClick(event) {
-  event.preventDefault();
-  refs.header.classList.remove('header-library');
-  refs.header.classList.add('header-search');
+    event.preventDefault();
+    refs.header.classList.remove('header-library');
+    refs.header.classList.add('header-search');
 }
 
 function onLibraryLinkClick(event) {
-  event.preventDefault();
-  refs.header.classList.remove('header-search');
-  refs.header.classList.add('header-library');
-  onLibraryWatchBtnClick();
+    event.preventDefault();
+    refs.header.classList.remove('header-search');
+    refs.header.classList.add('header-library');
+    onLibraryWatchBtnClick();
 }
 
 function onLibraryWatchBtnClick() {
-  refs.libraryWatchBtn.classList.remove('accent-btn');
-  refs.libraryWatchBtn.classList.add('accent-btn');
-  refs.libraryQueBtn.classList.remove('accent-btn');
+    refs.libraryWatchBtn.classList.remove('accent-btn');
+    refs.libraryWatchBtn.classList.add('accent-btn');
+    refs.libraryQueBtn.classList.remove('accent-btn');
 }
 
 function onLibraryQueBtnClick() {
-  refs.libraryQueBtn.classList.remove('accent-btn');
-  refs.libraryQueBtn.classList.add('accent-btn');
-  refs.libraryWatchBtn.classList.remove('accent-btn');
+    refs.libraryQueBtn.classList.remove('accent-btn');
+    refs.libraryQueBtn.classList.add('accent-btn');
+    refs.libraryWatchBtn.classList.remove('accent-btn');
 }
 
 function openTeamModal() {
-  window.addEventListener('keydown', checkKeyPress);
-  document.body.classList.add('modal-open');
+    window.addEventListener('keydown', checkKeyPress);
+    document.body.classList.add('modal-open');
 }
 
 function closeTeamModal() {
-  window.removeEventListener('keydown', checkKeyPress);
-  document.body.classList.remove('modal-open');
+    window.removeEventListener('keydown', checkKeyPress);
+    document.body.classList.remove('modal-open');
 }
 
 function checkKeyPress(event) {
-  if (event.code === 'Escape') {
-    closeTeamModal();
-  }
+    if (event.code === 'Escape') {
+        closeTeamModal();
+    }
 }
 
 function onBackdropClick(event) {
-  if (event.currentTarget === event.target) {
-    closeTeamModal();
-  }
+    if (event.currentTarget === event.target) {
+        closeTeamModal();
+    }
 }
 
 function onMoviesSearch(event) {
-  event.preventDefault();
-  const query = event.target.elements.query.value;
-  refs.cardsBox.innerHTML = '';
-  searchMovies(query);
+    event.preventDefault();
+    const query = event.target.elements.query.value;
+    refs.cardsBox.innerHTML = '';
+    searchMovies(query);
 }
-
