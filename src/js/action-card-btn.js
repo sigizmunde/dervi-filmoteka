2b0dcd62105cd5cb1eaa2d058d60dd9ffaafbfbe
Sherzod Abdullaev <perfekt.vision2019@gmail.com>
2022-06-15 00:10:09 +0200

    added the "cancel button"

--- src/js/action-card-btn.js
+++ src/js/action-card-btn.js
@@ -1,6 +1,8 @@
 import { moviesCashe } from './global.js';
+import { refs } from './global.js';
 import { DataStorage } from './data.js';
 const data = new DataStorage();
+export let timerID = null;
 
 // action btn on the movie card
 export function onQueueBtnCard(btn, id) {
@@ -8,12 +10,20 @@ export function onQueueBtnCard(btn, id) {
   const movieCard = btn.closest('.card');
 
   if (data.getQueue().find(item => item.id === id)) {
-    data.removeFromQueue(id);
-    movieCard.classList.remove('in-queue');
+    cancelBtnApperingAndPlacement(movieCard);
+
+    timerID = setTimeout(() => {
+      refs.cancelBtn.classList.add('is-hidden');
+      data.removeFromQueue(id);
+      movieCard.classList.remove('in-queue');
+      movieCard.remove();
+    }, 3000);
+
     return;
   }
 
   let movie = data.getWatched().find(item => item.id === id);
+  console.log(movie);
   if (movie) {
     data.removeFromWatched(id);
     movieCard.classList.remove('in-watched');
@@ -32,8 +42,17 @@ export function onWatchedBtnCard(btn, id) {
   const movieCard = btn.closest('.card');
 
   if (data.getWatched().find(item => item.id === id)) {
-    data.removeFromWatched(id);
-    movieCard.classList.remove('in-watched');
+    // data.removeFromWatched(id);
+    // movieCard.classList.remove('in-watched');
+
+    cancelBtnApperingAndPlacement(movieCard);
+
+    timerID = setTimeout(() => {
+      refs.cancelBtn.classList.add('is-hidden');
+      data.removeFromWatched(id);
+      movieCard.classList.remove('in-watched');
+      movieCard.remove();
+    }, 3000);
     return;
   }
 
@@ -49,4 +68,11 @@ export function onWatchedBtnCard(btn, id) {
   data.addToWatched(movie);
   movieCard.classList.add('in-watched');
   // add notify
+}
+
+function cancelBtnApperingAndPlacement(movieCard) {
+  refs.cancelBtn.classList.remove('is-hidden');
+  refs.cancelBtn.classList.add('cancel-animation');
+  refs.cancelBtn.style.top = `${movieCard.offsetTop}px`;
+  refs.cancelBtn.style.left = `${Math.floor(movieCard.offsetLeft - 1)}px`;
 }
