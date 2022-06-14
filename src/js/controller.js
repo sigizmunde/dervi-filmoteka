0758017db89270322be59d432aa3de1815970800
ShenyaLipinsky <shenyalipinskiy@gmail.com>
2022-06-14 08:00:31 +0300

    bugfix hide pagination on library page

--- src/js/controller.js
+++ src/js/controller.js
@@ -15,6 +15,7 @@ import { DataStorage } from './data';
 import { onQueueBtnCard, onWatchedBtnCard } from './action-card-btn';
 
 import { onClickScrollTop } from './scroll-to-top';
+import { hidePagination } from './pagination';
 
 const data = new DataStorage();
 
@@ -78,7 +79,6 @@ function onHomeLinkClick(event) {
   // location.reload();
   refs.header.classList.remove('header-library');
   refs.header.classList.add('header-search');
-  refs.pagination.classList.remove('on-empty-library');
   refs.cardsSection.classList.remove('empty-library');
   refs.cardsBox.classList.remove('hide-labels');
 
@@ -98,7 +98,7 @@ function onLibraryLinkClick(event) {
   refs.libraryQueBtn.classList.remove('accent-btn');
   refs.cardsSection.classList.remove('empty-main-library');
 
-  refs.pagination.classList.add('on-empty-library');
+  hidePagination();
   if (data.getWatched().length === 0) {
     refs.cardsSection.classList.add('empty-library');
   } else {
