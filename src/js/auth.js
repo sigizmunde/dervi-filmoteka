151232a7d43a98d81366def529a353d30535f14c
Oleksii Borshch <sergeevi44@gmail.com>
2022-06-15 09:57:12 +0300

    js login-signout

--- src/js/auth.js
+++ src/js/auth.js
@@ -27,35 +27,35 @@ export default class Auth {
       </div>
     </div>
     `;
-    this.loginBtn = document.getElementById('login');
-    this.signupBtn = document.getElementById('signup');
   }
 
   makeMarkup() {
     this.loginSignoutBtn.addEventListener('click', () => {
       document.querySelector('.movie-modal').classList.remove('is-hidden');
       document.querySelector('.modal-content').innerHTML = this.markup;
-    });
-    this.loginBtn.addEventListener('click', e => {
-      let parent = e.target.parentNode.parentNode;
-      Array.from(e.target.parentNode.parentNode.classList).find(element => {
-        if (element !== 'slide-up') {
-          parent.classList.add('slide-up');
-        } else {
-          this.signupBtn.parentNode.classList.add('slide-up');
-          parent.classList.remove('slide-up');
-        }
+      const loginBtn = document.getElementById('login');
+      const signupBtn = document.getElementById('signup');
+      loginBtn.addEventListener('click', e => {
+        let parent = e.target.parentNode.parentNode;
+        Array.from(e.target.parentNode.parentNode.classList).find(element => {
+          if (element !== 'slide-up') {
+            parent.classList.add('slide-up');
+          } else {
+            signupBtn.parentNode.classList.add('slide-up');
+            parent.classList.remove('slide-up');
+          }
+        });
       });
-    });
-    this.signupBtn.addEventListener('click', e => {
-      let parent = e.target.parentNode;
-      Array.from(e.target.parentNode.classList).find(element => {
-        if (element !== 'slide-up') {
-          parent.classList.add('slide-up');
-        } else {
-          this.loginBtn.parentNode.parentNode.classList.add('slide-up');
-          parent.classList.remove('slide-up');
-        }
+      signupBtn.addEventListener('click', e => {
+        let parent = e.target.parentNode;
+        Array.from(e.target.parentNode.classList).find(element => {
+          if (element !== 'slide-up') {
+            parent.classList.add('slide-up');
+          } else {
+            loginBtn.parentNode.parentNode.classList.add('slide-up');
+            parent.classList.remove('slide-up');
+          }
+        });
       });
     });
   }
